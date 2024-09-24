f323941e3f107dcea22cc79c9a8bd3ba237f9656
Kein Lika <klika@byui.edu>
2024-09-23 20:57:13 -0600

    Initial Commit

---
+++ Hello World/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Kein Lika on 9/23/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // Displaying Hello World.
+    std::cout << "Hello, World!\n";
+    return 0;
+}
